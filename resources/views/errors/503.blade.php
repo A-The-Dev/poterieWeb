2ea5e16cf826b507b83e7b3e0236b29787f46bbc
Nicola Filiatreault <202273631@cegepsherbrooke.qc.ca>
2025-12-31 15:54:54 -0500

    Update 503.blade.php

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,8 +1,21 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Under Maintenance</title>
+    <style>
+        body { text-align: center; padding: 150px; background-color: #f4f4f4; font-family: Arial, sans-serif; }
+        h1 { font-size: 50px; color: #333; }
+        p { font-size: 20px; color: #666; }
+    </style>
+</head>
+<body
     <div class="lg:content-height py-16 lg:px-12 flex flex-col justify-center">
         <h1 class="text-center titre1-dark">Bientôt disponible!</h1>
         <p class="mt-10">Ce site est temporairement indisponible. Veuillez réessayer plus tard.</p>
 
         <p class="mt-10">Des travaux sont en cours. À bientôt pour une version améliorée!</p>
     </div>
-</x-app-layout>
+</body>
+</html>
