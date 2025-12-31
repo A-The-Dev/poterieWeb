87b97dbdd604f3c76b8a4591caced3102a31cb23
Nicola Filiatreault <202273631@cegepsherbrooke.qc.ca>
2025-12-31 15:48:00 -0500

    Create 503.blade.php

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="lg:content-height py-16 lg:px-12 flex flex-col justify-center">
+        <h1 class="text-center titre1-dark">Bientôt disponible!</h1>
+        <p class="mt-10">Ce site est temporairement indisponible. Veuillez réessayer plus tard.</p>
+
+        <p class="mt-10">Des travaux sont en cours. À bientôt pour une version améliorée!</p>
+    </div>
+</x-app-layout>
